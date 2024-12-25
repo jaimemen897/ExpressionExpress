f4c235dfbb1c30d4f3dea0364162c7c0e8588c2b
jaimemen <jaimemen897@gmail.com>
2024-12-25 14:52:43 +0100

    Se añade cronómetro

--- src/index.js
+++ src/index.js
@@ -1,19 +1,97 @@
-var words = ['palabra1', 'palabra2', 'palabra3', 'palabra4', 'palabra5'];
-var currentIndex = 0;
+var words = ['PARCHIS', 'CLUEDO', 'LANGOSTA', 'DOMINICAL', 'ACERO', 'PERFUME', 'MINUTO', 'CANARIAS', 'PASTA ITALIANA', 'ESTACIÓN', 'VIETNAM', 'BARBERO', 'AJEDREZ', 'COMIDA RÁPIDA', 'HELADO', 'CARA', 'FISCAL', 'CARDENAL', 'TROMPETA', 'SOLTERONA', 'DEBER', 'LOS ANGELES', 'PUENTE LEVADIZO', 'OREJERAS', 'PEREGRINO', 'TOCAR', 'MOZART', 'DIADEMA', 'SODA', 'PATO DONALD', 'BUCEAR', 'AIRE', 'PARRILLA', 'LEOPARDO', 'FUERA DE LÍMITE', 'SONAJERO', 'ESCLAVO', 'EMPUJAR', 'REMO', 'SANTO', 'FANTASMA', 'DEBATE', 'SONROJARSE', 'LOBO DE MAR', 'PROPAGANDA', 'LUCES DE NEÓN', 'RAYOS X', 'POSTER', 'SEVILLANAS', 'BATMAN', 'CAFE', 'ATAÚD', 'INVENTOR', 'ESTÚPIDO', 'BALLENA', 'ESTÓMAGO', 'ALGODÓN AZÚCAR', 'VIERNES', 'MALABARISTA', 'BOLA NAFTALINA', 'PLEGAR', 'ALTO'];
+var currentIndex = Math.floor(Math.random() * words.length);
+var currentTime = 90;
+var interval = setInterval(function () {
+    nextTime();
+    displayClock();
+}, 1000);
+var isIntervalRunning = false;
 function displayWord() {
     var wordDisplay = document.getElementById("word-display");
+    var alertContainer = document.getElementById("alert-container");
+    if (words.length === 0) {
+        if (alertContainer) {
+            alertContainer.classList.remove("hidden");
+        }
+        return;
+    }
     if (wordDisplay) {
         wordDisplay.innerText = words[currentIndex];
+        words.splice(currentIndex, 1);
+    }
+}
+function displayClock() {
+    var seconds = document.getElementById("seconds");
+    if (seconds) {
+        console.log(currentTime);
+        seconds.innerText = currentTime.toString() + "s";
     }
 }
 function nextWord() {
-    console.log('next word');
-    if (currentIndex < words.length - 1) {
-        return words[currentIndex++];
+    if (!isIntervalRunning) {
+        resume();
+    }
+    if (words.length > 0) {
+        currentIndex = Math.floor(Math.random() * words.length);
     }
     else {
         currentIndex = 0;
-        return words[currentIndex];
+    }
+}
+function nextTime() {
+    if (currentTime > 0) {
+        return currentTime--;
+    }
+    else {
+        currentTime = 90;
+        return currentTime;
+    }
+}
+function pause() {
+    if (interval !== null) {
+        clearInterval(interval);
+        isIntervalRunning = false;
+        updateButtonStyles();
+    }
+}
+function resume() {
+    if (!isIntervalRunning) {
+        interval = setInterval(function () {
+            nextTime();
+            displayClock();
+        }, 1000);
+        isIntervalRunning = true;
+        updateButtonStyles();
+    }
+}
+function resetClock() {
+    currentTime = 90;
+    pause();
+    displayClock();
+}
+function updateButtonStyles() {
+    var startButton = document.getElementById("start");
+    var pauseButton = document.getElementById("pause");
+    if (isIntervalRunning) {
+        if (startButton) {
+            startButton.classList.remove("bg-green-500");
+            startButton.classList.add("bg-gray-800");
+        }
+        if (pauseButton) {
+            pauseButton.classList.remove("bg-gray-800");
+            pauseButton.classList.add("bg-black");
+        }
+    }
+    else {
+        if (startButton) {
+            startButton.classList.remove("bg-black");
+            startButton.classList.add("bg-green-500");
+        }
+        if (pauseButton) {
+            pauseButton.classList.remove("bg-black");
+            pauseButton.classList.add("bg-gray-800");
+        }
     }
 }
 displayWord();
+resetClock();
--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,108 @@
-let words = ['palabra1', 'palabra2', 'palabra3', 'palabra4', 'palabra5'];
-let currentIndex = 0;
+let words = ['PARCHIS', 'CLUEDO', 'LANGOSTA', 'DOMINICAL', 'ACERO', 'PERFUME', 'MINUTO', 'CANARIAS', 'PASTA ITALIANA', 'ESTACIÓN', 'VIETNAM', 'BARBERO', 'AJEDREZ', 'COMIDA RÁPIDA', 'HELADO', 'CARA', 'FISCAL', 'CARDENAL', 'TROMPETA', 'SOLTERONA', 'DEBER', 'LOS ANGELES', 'PUENTE LEVADIZO', 'OREJERAS', 'PEREGRINO', 'TOCAR', 'MOZART', 'DIADEMA', 'SODA', 'PATO DONALD', 'BUCEAR', 'AIRE', 'PARRILLA', 'LEOPARDO', 'FUERA DE LÍMITE', 'SONAJERO', 'ESCLAVO', 'EMPUJAR', 'REMO', 'SANTO', 'FANTASMA', 'DEBATE', 'SONROJARSE', 'LOBO DE MAR', 'PROPAGANDA', 'LUCES DE NEÓN', 'RAYOS X', 'POSTER', 'SEVILLANAS', 'BATMAN', 'CAFE', 'ATAÚD', 'INVENTOR', 'ESTÚPIDO', 'BALLENA', 'ESTÓMAGO', 'ALGODÓN AZÚCAR', 'VIERNES', 'MALABARISTA', 'BOLA NAFTALINA', 'PLEGAR', 'ALTO'];
+
+let currentIndex = Math.floor(Math.random() * words.length);
+let currentTime = 90;
+let interval = setInterval(() => {
+    nextTime();
+    displayClock();
+}, 1000);
+let isIntervalRunning = false;
 
 function displayWord() {
     let wordDisplay = document.getElementById("word-display");
-    if (wordDisplay){
+    let alertContainer = document.getElementById("alert-container");
+
+    if (words.length === 0) {
+        if (alertContainer) {
+            alertContainer.classList.remove("hidden");
+        }
+        return;
+    }
+
+    if (wordDisplay) {
         wordDisplay.innerText = words[currentIndex];
+        words.splice(currentIndex, 1);
+    }
+}
+
+function displayClock(){
+    let seconds = document.getElementById("seconds");
+    if (seconds){
+        console.log(currentTime);
+        seconds.innerText = currentTime.toString() + "s";
     }
 }
 
 function nextWord() {
-    console.log('next word');
-    if (currentIndex < words.length - 1) {
-        return words[currentIndex++];
+    if (!isIntervalRunning) {
+        resume();
+    }
+    if (words.length > 0) {
+        currentIndex = Math.floor(Math.random() * words.length);
     } else {
         currentIndex = 0;
-        return words[currentIndex];
     }
 }
 
+function nextTime() {
+    if (currentTime > 0) {
+        return currentTime--;
+    } else {
+        currentTime = 90;
+        return currentTime;
+    }
+}
+
+function pause() {
+    if (interval !== null) {
+        clearInterval(interval);
+        isIntervalRunning = false;
+        updateButtonStyles();
+    }
+}
+
+function resume() {
+    if (!isIntervalRunning) {
+        interval = setInterval(() => {
+            nextTime();
+            displayClock();
+        }, 1000);
+        isIntervalRunning = true;
+        updateButtonStyles();
+    }
+}
+
+function resetClock() {
+    currentTime = 90;
+    pause();
+    displayClock();
+}
+
+function updateButtonStyles() {
+    const startButton = document.getElementById("start");
+    const pauseButton = document.getElementById("pause");
+
+    if (isIntervalRunning) {
+        if (startButton) {
+            startButton.classList.remove("bg-green-500");
+            startButton.classList.add("bg-gray-800");
+        }
+        if (pauseButton) {
+            pauseButton.classList.remove("bg-gray-800");
+            pauseButton.classList.add("bg-black");
+        }
+    } else {
+        if (startButton) {
+            startButton.classList.remove("bg-black");
+            startButton.classList.add("bg-green-500");
+        }
+        if (pauseButton) {
+            pauseButton.classList.remove("bg-black");
+            pauseButton.classList.add("bg-gray-800");
+        }
+    }
+}
+
+
 displayWord();
+resetClock();
